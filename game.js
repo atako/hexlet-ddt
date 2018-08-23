cffc4344639e8bc0f25a1c739cd30be67cf7804b
Dmitry Ryumkin <riumkin@gmail.com>
2018-08-23 13:41:12 +1000

    add inversion

--- game.js
+++ game.js
@@ -1,15 +1,16 @@
 import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
 import { cons as consList, l, random, head, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
 
-const run = (player1, player2, cards) => {
+const run = (player1, player2, cards, customRandom) => {
   const iter = (health1, name1, health2, name2, order, log) => {
-    // BEGIN (write your solution here)
     if (health1 <= 0) {
       return consList(cons(car(head(log)), `${name1} был убит`), log);
     }
-    const card = random(cards);
+    // BEGIN (write your solution here)
+    const card = customRandom(cards);
+    // END
     const cardName = car(card);
-    const damage = cdr(card)();
+    const damage = cdr(card)(health2);
     const newHealth = health2 - damage;
 
     const message = `Игрок '${name1}' применил '${cardName}'
@@ -22,7 +23,6 @@ const run = (player1, player2, cards) => {
     }
     const newLog = consList(stats, log);
     return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
-    // END
   };
 
   const startHealth = 10;
@@ -30,6 +30,9 @@ const run = (player1, player2, cards) => {
   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
 };
 
-export default cards =>
-  (name1, name2) =>
-    run(name1, name2, cards);
+// BEGIN (write your solution here)
+export default (cards, func = random) => {
+  const launch = (name1, name2) => run(name1, name2, cards, func);
+  return launch;
+}
+// END
